57f3d4ac5fff74a3e800e5700100cf038461c79b
Volodymyr Matyukh <vmatyukh@softserveinc.com>
2016-09-15 12:24:04 +0300

    added acces controls to app.js

--- app.js
+++ app.js
@@ -28,6 +28,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', "*");
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	next();
+})
+
 
 // setup routes
 let routes = require('./routes');
